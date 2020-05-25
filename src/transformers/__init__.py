ba458da4bbdb68612e2fd12eac504877740db0bc
Patrick von Platen <patrick.v.platen@gmail.com>
2020-05-25 10:33:44 +0200

    make style

--- src/transformers/__init__.py
+++ src/transformers/__init__.py
@@ -334,7 +334,7 @@ if is_torch_available():
     from .data.data_collator import DefaultDataCollator, DataCollator, DataCollatorForLanguageModeling
     from .data.datasets import GlueDataset, TextDataset, LineByLineTextDataset, GlueDataTrainingArguments
 
-# Benchmarks
+    # Benchmarks
     from .benchmark import PyTorchBenchmarks, PyTorchBenchmarkArguments
 
 # TensorFlow
@@ -510,9 +510,8 @@ if is_tf_available():
     # Trainer
     from .trainer_tf import TFTrainer
 
-# Benchmarks
+    # Benchmarks
     from .benchmark import TensorflowBenchmarkArguments
-
 
 
 if not is_tf_available() and not is_torch_available():
